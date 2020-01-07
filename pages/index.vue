0aadda47e1fb0582652d50451a1989768948e975
kkumarasamy <kkumarasamy@edit-place.com>
2020-01-07 20:35:12 +0530

    Added json-server to watch db.json

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 <script>
+import EventCard from '@/components/EventCard'
 
 export default {
   head() {
     return {
-      title: 'Event Listing - Real World Events',
+      title: 'Event Listing',
       meta: [
         {
           hid: 'description',
@@ -17,6 +25,25 @@ export default {
         }
       ]
     }
-  }
+  },
+  // asyncData(context) {
+    // return context.$axios.get('http://localhost:3400/events')
+  asyncData({ $axios, error }) {
+    return $axios.get('http://localhost:3000/events')
+      .then(res => {
+        return {
+          events: res.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.'
+        })
+      })
+  },
+  components: {
+    EventCard
+  },
 }
 </script>
