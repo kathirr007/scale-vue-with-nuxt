091f96724032ce7c676c29e8b92e9ae6c8c98271
kathirr007 <kkumarasamy@edit-place.com>
2020-01-08 01:05:27 +0530

    Updated json server to github repo link

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:3000',
+  // baseURL: process.env.BASE_URL || 'http://localhost:3000',
+  baseURL: process.env.BASE_URL || 'https://my-json-server.typicode.com/kathirr007/scale-vue-with-nuxt',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
